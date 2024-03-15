d5124dbeee6313afb83e9ce5526aac5d75f4cfe3
alanvf1 <alan.varela404@gmail.com>
2024-03-14 23:12:21 -0300

    bug: input step button forwardRef

--- src/components/Input/InputStepButton.tsx
+++ src/components/Input/InputStepButton.tsx
@@ -1,23 +1,28 @@
 import { Minus, Plus } from "phosphor-react";
 import { InputStepDownButton, InputStepUpButton } from "./styles";
-import { ComponentProps } from "react";
+import { ComponentProps, Ref, forwardRef } from "react";
 
 interface InputStepButtonProps extends ComponentProps<"button"> {
   step: "up" | "down";
 }
 
-export function InputStepButton({ step, ...rest }: InputStepButtonProps) {
-  if (step == "up") {
-    return (
-      <InputStepUpButton type="button" {...rest}>
-        <Plus size={14} />
-      </InputStepUpButton>
-    );
-  } else {
-    return (
-      <InputStepDownButton type="button" {...rest}>
-        <Minus size={14} />
-      </InputStepDownButton>
-    );
+export const InputStepButton = forwardRef(
+  (
+    { step, ...rest }: InputStepButtonProps,
+    forwardedRef: Ref<HTMLButtonElement>
+  ) => {
+    if (step == "up") {
+      return (
+        <InputStepUpButton type="button" {...rest} ref={forwardedRef}>
+          <Plus size={14} />
+        </InputStepUpButton>
+      );
+    } else {
+      return (
+        <InputStepDownButton type="button" {...rest} ref={forwardedRef}>
+          <Minus size={14} />
+        </InputStepDownButton>
+      );
+    }
   }
-}
+);
